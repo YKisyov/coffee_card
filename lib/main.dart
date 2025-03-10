df3e4794ba321ad52dc43396c27ed67875d3cb58
Yordan Kisyov <javacodex@gmail.com>
2025-03-10 20:37:42 +0200

    Playing around with some column widgets.

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,44 @@
+import 'package:coffee_card/Home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text(
-            "> > My coffee ID < <",
-            style: TextStyle(color: Colors.white60),
-          ),
-          backgroundColor: Colors.brown[700],
-        ),
-        body: Home(),
-      ),
-    ),
-  );
+  runApp(MaterialApp(home: Sandbox()));
 }
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class Sandbox extends StatelessWidget {
+  const Sandbox({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.orange,
-      padding: EdgeInsets.all(50),
-      child: Text("Драсти Нинджа",
-        style: TextStyle(
-          fontSize: 32,
-          letterSpacing: 18,
-          fontStyle: FontStyle.italic,
-        ),),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("S A N D   B O X"),
+        backgroundColor: Colors.amber,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            alignment: Alignment.center,
+            width: 100,
+            color: Colors.red[300],
+            child: Text("One, One, One, One, One, One, One, One, One.", style: TextStyle(fontSize: 30)),
+          ),
+          Container(
+            alignment: Alignment.center,
+            width: 200,
+            color: Colors.green[300],
+            child: Text("Two", style: TextStyle(fontSize: 40)),
+          ),
+          Container(
+            alignment: Alignment.center,
+            width: 300,
+            color: Colors.blue[300],
+            child: Text("Three", style: TextStyle(fontSize: 50)),
+          ),
+        ],
+      ),
     );
   }
 }
